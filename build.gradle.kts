dda542c0ac1e728ff8fc0a0aa8dd3ac58965a1c7
Su5eD <su5ed@outlook.com>
2023-06-20 21:43:11 +0200

    Setup publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,17 @@ import net.minecraftforge.gradle.common.util.RunConfig
 
 plugins {
     java
+    `maven-publish`
     id("net.minecraftforge.gradle") version "[6.0,6.2)"
 }
 
-group = "dev.su5ed.sinytra"
-version = "1.0"
-
 // TODO Api compat check
 val versionMc: String by rootProject
+val versionLoaderUpstream: String by rootProject
+val implVersion = "1.0"
+
+group = "dev.su5ed.sinytra"
+version = "$implVersion+$versionLoaderUpstream"
 
 minecraft {
     mappings("official", versionMc)
@@ -33,20 +36,20 @@ minecraft {
 }
 
 sourceSets {
-	main {
-		java {
+    main {
+        java {
             srcDirs("src/main/java", "src/main/legacyJava")
         }
-	}
+    }
 }
 
 repositories {
     mavenCentral()
-    maven { 
+    maven {
         name = "MinecraftForge"
         url = uri("https://maven.minecraftforge.net")
     }
-    maven { 
+    maven {
         name = "FabricMC"
         url = uri("https://maven.fabricmc.net")
     }
@@ -54,9 +57,33 @@ repositories {
 
 dependencies {
     minecraft(group = "net.minecraftforge", name = "forge", version = "$versionMc-45.0.64")
-    
+
     implementation("net.minecraftforge:fmlloader:1.19.4-45.1.0")
     implementation("net.minecraftforge:srgutils:0.5.4")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+}
+
+tasks.withType<GenerateModuleMetadata> {
+    isEnabled = false
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            fg.component(this)
+        }
+    }
+
+    repositories {
+        maven {
+            name = "Su5eD"
+            url = uri("https://maven.su5ed.dev/releases")
+            credentials {
+                username = System.getenv("MAVEN_USER") ?: "not"
+                password = System.getenv("MAVEN_PASSWORD") ?: "set"
+            }
+        }
+    }
 }
