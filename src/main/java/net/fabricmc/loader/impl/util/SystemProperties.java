772a645410e7bd9c6062013e396745370dc3cb1a
Player <player@player.to>
2025-06-29 00:52:28 +0200

    Add fabric.gameLibraries to explicitly specify game libraries instead of using the class path
    (cherry picked from commit 41de1d9cd7584c4f7d368af8f9f6e70f344395c6)

--- src/main/java/net/fabricmc/loader/impl/util/SystemProperties.java
+++ src/main/java/net/fabricmc/loader/impl/util/SystemProperties.java
@@ -32,6 +32,9 @@ public final class SystemProperties {
 	public static final String GAME_JAR_PATH = "fabric.gameJarPath";
 	public static final String GAME_JAR_PATH_CLIENT = "fabric.gameJarPath.client";
 	public static final String GAME_JAR_PATH_SERVER = "fabric.gameJarPath.server";
+	// game library paths, replaces lookup from class path if present
+	// paths separated by path separator, @ prefix for meta-file with each line referencing an actual file)
+	public static final String GAME_LIBRARIES = "fabric.gameLibraries";
 	// set the game version for the builtin game mod/dependencies, bypassing auto-detection
 	public static final String GAME_VERSION = "fabric.gameVersion";
 	// fallback log file for the builtin log handler (dumped on exit if not replaced with another handler)
@@ -44,7 +47,8 @@ public final class SystemProperties {
 	public static final String REMAP_CLASSPATH_FILE = "fabric.remapClasspathFile";
 	// class path groups to map multiple class path entries to a mod (paths separated by path separator, groups by double path separator)
 	public static final String PATH_GROUPS = "fabric.classPathGroups";
-	// system level libraries, matching code sources will not be assumed to be part of the game or mods and remain on the system class path (paths separated by path separator)
+	// system level libraries, matching code sources will not be assumed to be part of the game or mods and remain on the system class path
+	// paths separated by path separator, @ prefix for meta-file with each line referencing an actual file)
 	public static final String SYSTEM_LIBRARIES = "fabric.systemLibraries";
 	// throw exceptions from entrypoints, discovery etc. directly instead of gathering and attaching as suppressed
 	public static final String DEBUG_THROW_DIRECTLY = "fabric.debug.throwDirectly";
