c523a89d234945638ad659de3fcf9912bf1b6793
Player <player@player.to>
2025-07-02 19:26:49 +0200

    Add fabric.mappingPath system property, refactor mapping reading
    (cherry picked from commit 9d1bbd971d2fb90c07a775b9e52b895c510e46c7)

--- src/main/java/net/fabricmc/loader/impl/util/SystemProperties.java
+++ src/main/java/net/fabricmc/loader/impl/util/SystemProperties.java
@@ -20,6 +20,8 @@ public final class SystemProperties {
 	// whether fabric loader is running in a development environment / mode, affects class path mod discovery, remapping, logging, ...
 	public static final String DEVELOPMENT = "fabric.development";
 	public static final String SIDE = "fabric.side";
+	// file to source mappings from, defaults to mappings/mappings.tiny on the class path
+	public static final String MAPPING_PATH = "fabric.mappingPath";
 	// mapping namespace used by the game, defaults to named if DEVELOPMENT is set or official otherwise
 	public static final String GAME_MAPPING_NAMESPACE = "fabric.gameMappingNamespace";
 	// mapping namespace to use at runtime, defaults to named if DEVELOPMENT is set or intermediary otherwise
